ddc9052d188633ba23133cb72659c973417035ca
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2023-04-22 16:37:09 -0400

    Add files via upload

--- Deployment/QuadEncoder.py
+++ Deployment/QuadEncoder.py
@@ -1,6 +1,18 @@
+"""
+QuadEncoder class to obtain position and velocity from an incremental encoder.
+This class calculates the distance traveled and velocity of a wheel with the help of an incremental encoder. 
+It provides methods to obtain the net distance traveled by the wheel between calls of the function, 
+the average velocity of the wheel over a specified time window size, and the instantaneous velocity of the wheel 
+between the last two encoder position updates. The class requires input parameters such as the encoder pins, gear ratio, 
+wheel radius, and encoder CPR (counts per revolution) for proper operation. The class also includes a GPIO cleanup 
+function for resetting GPIO pin modes.
+"""
+
 import time
 import Jetson.GPIO as GPIO
+import math.pi as pi
 
+# Quadrature Encoder class to obtain position and velocity from an incremental encoder
 class QuadEncoder:
     def __init__(self, encoder_A_pin, encoder_B_pin, gear_ratio, wheel_radius, encoder_cpr):
         self.encoder_A_pin = encoder_A_pin
@@ -16,36 +28,49 @@ class QuadEncoder:
         GPIO.setup(encoder_B_pin, GPIO.IN)
         GPIO.add_event_detect(encoder_A_pin, GPIO.BOTH, callback=self.encoder_callback)
 
+    # Interrupt to update the encoder count
     def encoder_callback(self, channel):
         if GPIO.input(self.encoder_A_pin) == GPIO.input(self.encoder_B_pin):
             self.encoder_pos += 1
         else:
             self.encoder_pos -= 1
 
+    # Calculates net distance traveled by the wheel between calls of this function
+    def get_distance_since_last_call(self):
+        current_pos = self.encoder_pos
+        distance_per_count = 2 * pi * self.wheel_radius / (self.encoder_cpr * 4 * self.gear_ratio)
+        distance_traveled = (current_pos - self.last_pos) * distance_per_count
+        self.last_pos = current_pos
+        return distance_traveled
+
+    # Calculate average velocity of a wheel over time window size parameter, duration
     def get_wheel_velocity_duration(self, duration):
         start_pos = self.encoder_pos
-        start_time = time.time()
+        start_time = time.monotonic()
         time.sleep(duration)
         end_pos = self.encoder_pos
-        end_time = time.time()
+        end_time = time.monotonic()
         counts_per_second = (end_pos - start_pos) / (end_time - start_time)
         counts_per_revolution = self.encoder_cpr * 4
-        wheel_circumference = 2 * 3.141592 * self.wheel_radius
+        wheel_circumference = 2 * pi * self.wheel_radius
         gear_ratio_eff = self.gear_ratio * 11/33
         wheel_velocity = counts_per_second * wheel_circumference * gear_ratio_eff / counts_per_revolution
         return wheel_velocity
 
+    # Calculate the instantaneous velocity of a wheel between the last two encoder position updates
     def get_instantaneous_velocity(self):
         current_pos = self.encoder_pos
-        current_time = time.time()
+        current_time = time.monotonic()
         counts_per_second = (current_pos - self.last_pos) / (current_time - self.last_time)
         counts_per_revolution = self.encoder_cpr * 4
-        wheel_circumference = 2 * 3.141592 * self.wheel_radius
+        wheel_circumference = 2 * pi * self.wheel_radius
         gear_ratio_eff = self.gear_ratio * 11/33
         wheel_velocity = counts_per_second * wheel_circumference * gear_ratio_eff / counts_per_revolution
         self.last_pos = current_pos
         self.last_time = current_time
         return wheel_velocity
 
+    # Function to reset GPIO pin modes
     def cleanup(self):
-        GPIO.cleanup()
+        GPIO.cleanup(self.encoder_A_pin)
+        GPIO.cleanup(self.encoder_B_pin)
