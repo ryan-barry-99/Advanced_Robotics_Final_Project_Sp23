f1d34e12feab47f83621e92013c7c8a5adccf9f4
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2023-04-24 13:25:39 -0400

    Add files via upload

---
+++ Deployment/pinout.py
@@ -0,0 +1,11 @@
+# pinout.py
+# A dictionary containing the GPIO pin numbers for the robot
+
+pins = {
+    'encoder_1_a': 11,
+    'encoder_1_b': 13,
+    'encoder_2_a': 15,
+    'encoder_2_b': 16,
+    'encoder_3_a': 18,
+    'encoder_3_b': 22
+}
