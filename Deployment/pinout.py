76aa20e550ad55fee1b491dc8095776cfbf60f22
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2023-04-29 19:53:06 -0400

    Update pinout.py

--- Deployment/pinout.py
+++ Deployment/pinout.py
@@ -3,9 +3,18 @@
 
 pinout = {
     'encoder_1_a': 11,
-    'encoder_1_b': 13,
+    'encoder_1_b': 12,
     'encoder_2_a': 15,
     'encoder_2_b': 16,
     'encoder_3_a': 18,
-    'encoder_3_b': 22
+    'encoder_3_b': 19,
+    'motor_1_dir_a': 21,
+    'motor_1_dir_b': 22,
+    'motor_1_pwm': 23,
+    'motor_2_dir_a': 24,
+    'motor_2_dir_b': 26,
+    'motor_2_pwm': 27,
+    'motor_3_dir_a': 28,
+    'motor_3_dir_b': 29,
+    'motor_3_pwm': 31
 }
